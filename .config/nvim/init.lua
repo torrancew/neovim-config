ea70def85fc816cfa65982d820c4617e0a5c14df
Tray Torrance <torrancew@gmail.com>
2025-04-09 21:35:57 -0700

    Enable extra colorschemes and better golang support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,7 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.cmd.colorscheme 'selenized'
+vim.cmd.colorscheme 'nightfox'
 vim.keymap.set('n', '<localleader>ff', ':Telescope find_files<CR>', {silent=true})
 
 vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
@@ -43,3 +43,25 @@ smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-T
 " If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
 ]])
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
+})
--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,3 +1,4 @@
 return {
   {'calind/selenized.nvim', lazy = true},
+  {'EdenEast/nightfox.nvim', lazy = true},
 }
