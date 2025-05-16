acebeb68c14b8b4678145d382e09fb80aaaa9bea
Tray Torrance <torrancew@gmail.com>
2025-05-15 21:02:51 -0700

    Update from main workstation

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,13 +5,28 @@ return {
       local caps = require('cmp_nvim_lsp').default_capabilities()
 
       for _, lsp in ipairs({
-        "asm_lsp", "clangd", "gopls", "pylsp", "pyright", 
+        "asm_lsp", "clangd", "gopls",
         "openscad_lsp", "tinymist", "vale_ls"
       }) do
         require('lspconfig')[lsp].setup {
           capabilities = caps,
         }
       end
+
+      -- Use Ruff for linting, formatting and import sorting
+      -- and Pyright for other LSP features
+      require('lspconfig').ruff.setup {
+      }
+      require('lspconfig').pyright.setup {
+        settings = {
+          pyright = {
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = { ignore = { '*' } },
+          }
+        },
+      }
     end,
     dependencies = {
       { 'mfussenegger/nvim-dap' },
